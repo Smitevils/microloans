d706447e2b04d0c7541afa16aa986637f087ab6d
smitevils <smitevils@yandex.ru>
2015-10-14 15:42:50 +0300

    Добавил JQ и файл стилей

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Сервис Микрозаймов</title>
+	<!-- Meta Data / Мета данные -->
+	<meta name="description" content="Бесплатные онлайн учебники по HTML, CSS, JavaScript, jQuery, HDOM и AJAX" />
+	<meta name="keywords" content="HTML, XHTML, CSS, JavaScript, jQuery, HDOM, AJAX" />
+	<!-- Styles / Стили -->
+	<link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+</head>
+<body>
+	<!-- Scripts - Скрипты -->
+	<!-- JQ 1.11.3 -->
+	<script src="assets/scripts/jquery-1.11.3.min.js"></script>
+	<!-- Own Script / Наш скрипт -->
+	<script src="assets/scripts/oun-script.js"></script>
+	<!-- /Scripts - Скрипты -->
+</body>
+</html>
